6abfb719a18c60862e74836b2e0070426b524e11
West <accounts@hoof.dev>
2021-03-15 16:55:01 -0400

    Initialize parser

---
+++ src/resumeParser/resumeParser.py
@@ -0,0 +1,25 @@
+from io import StringIO
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfparser import PDFParser
+
+def fetchText(filename):
+    output_string = StringIO()
+    with open(filename, 'rb') as f:
+        parser = PDFParser(f)
+        doc = PDFDocument(parser)
+        rsrcmgr = PDFResourceManager()
+        device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+        for page in PDFPage.create_pages(doc):
+            interpreter.process_page(page)
+
+    print(output_string.getvalue())
+    return output_string.getvalue()
+
+
+if __name__ == "__main__":
+    fetchText(input("Enter resume file path\n"))
