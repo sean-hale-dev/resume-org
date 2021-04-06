ef1459a672dfe5703df613d6d2898e1fee3d472b
West <accounts@hoof.dev>
2021-04-05 21:59:12 -0400

    Added comments

--- src/server/search.js
+++ src/server/search.js
@@ -1,6 +1,38 @@
 const { MongoClient, ObjectID } = require('mongodb');
 const dotenv = require('dotenv').config();
 
+// Performs intersection operation between
+// called set and otherSet
+//
+// FUNCTION GRABBED FROM https://www.geeksforgeeks.org/sets-in-javascript/
+Set.prototype.intersection = function (otherSet) {
+  // creating new set to store intersection
+  var intersectionSet = new Set();
+
+  // Iterate over the values
+  for (var elem of otherSet) {
+    // if the other set contains a
+    // similar value as of value[i]
+    // then add it to intersectionSet
+    if (this.has(elem)) intersectionSet.add(elem);
+  }
+
+  // return values of intersectionSet
+  return intersectionSet;
+};
+
+/*
+ * VOCAB:
+ * query -> A string of skills joined by logical operators and grouped in parenthesis ( &, *, | ) i.e. ((a & b) | (c * d))
+ * chunk -> An element of a query which is enclosed in parenthesis i.e. ( a & b )
+ * component -> A skill in a chunk i.e. (a)
+ * macro -> A substituted chunk in a query represented by the form "$<macroID>$". i.e. $1$ = a & b --> ($1$ | (c * d))
+ */
+
+/*
+ * Function responsible for parsing an individual chunk of a query into a query object
+ * query obj = { operation: "<and/or/xor>", "containsMacros": <true/false>, components:[ {token: <name of component> isMacro:<true/false>} ]}
+ */
 function parseString(group) {
   var searchParams = {
     operation: '',
@@ -8,9 +40,11 @@ function parseString(group) {
     components: [],
   };
 
+  // Select skill tokens from raw chunk
   tokenSelect = /([\w\d!.\$][\w\d .+-]*[\w\d\$+-.])|(?<=[ \|&\*])\w(?=[ \|&\*])|(?<=[ \|&\*])\w|\w(?=[ \|&\*])/gi;
   group = group.replace(/! /g, '!');
   tokens = group.match(tokenSelect);
+  // If tokens were selected, parse and store
   if (tokens != null)
     tokens.map((token) => {
       let component = {};
@@ -28,12 +62,16 @@ function parseString(group) {
   return searchParams;
 }
 
+/*
+ * Function responsible for parsing query strings
+ */
 function parseQuery(query) {
   var chunks = [];
 
   var curID = 1;
   var managedString = query;
 
+  // Seperate and parse a chunk, store the chunk obj, and then replace the chunk with a macro in the query
   function parseChunk(startIDX, endIDX) {
     if (/^[^\(\)]*$/g.test(managedString.slice(startIDX + 1, endIDX))) {
       chunks.splice(curID, 0, {
@@ -53,6 +91,7 @@ function parseQuery(query) {
     var startChunkIDX = -1;
     var endChunkIDX = -1;
 
+    // Detect and isolate grouped chunk
     for (let i = 0; i < managedString.length; i++) {
       if (managedString[i] == '(') startChunkIDX = i;
       if (managedString[i] == ')') {
@@ -61,6 +100,7 @@ function parseQuery(query) {
       }
     }
 
+    // Handle errors in chunk detection
     if (
       endChunkIDX <= startChunkIDX ||
       startChunkIDX == -1 ||
@@ -73,18 +113,23 @@ function parseQuery(query) {
     parseChunk(startChunkIDX, endChunkIDX);
   }
 
+  // Parse the query while groups remain
   while (/[\(\)]/g.test(managedString)) {
     parseChunk(0, managedString.length);
   }
   curID = 0;
+  // Parse the root macro
   parseChunk(0, managedString.length);
   return chunks;
 }
 
+// Function which takes in a parsed query obj and calculates the query from mongo
 async function handleQuery(queryObj) {
   var respTable = {};
   var keys = [];
   var macrosTable = {};
+
+  // Construct initial lookup tables
   queryObj.map((obj) => {
     obj.ops.components.map((component) => {
       if (!component.isMacro) keys.push(component.token);
@@ -92,6 +137,7 @@ async function handleQuery(queryObj) {
     });
   });
 
+  // Grab resume _id's from mongo with the keyset
   const fetchFromMongo = async (tokenArr) => {
     const mongoClient = new MongoClient(process.env.MONGO_URI, {
       useNewUrlParser: true,
@@ -104,7 +150,7 @@ async function handleQuery(queryObj) {
       tokenArr.map((t) => mgdbQueryObj['$or'].push({ name: t }));
       var resRet = await mongoClient
         .db('resume_org')
-        .collection('skill_assoc')
+        .collection('searchTesting')
         .find(mgdbQueryObj, { _id: 0 })
         .toArray();
     } finally {
@@ -113,31 +159,41 @@ async function handleQuery(queryObj) {
     }
   };
 
+  // Parse mongo response into sets and store
   var resp = await fetchFromMongo(keys);
   resp.map((respObj) => (respTable[respObj.name] = new Set(respObj.resumes)));
   console.log(respTable);
 
+  // Recursivly calculate a chunk, starting with non-macro chunks and working up to the root chunk.
   const resolveChunk = (chunk) => {
+    console.log(chunk);
+    // If chunk has unresolved macros, first resolve before continuing
     chunk.ops.components.map((component) => {
       if (component.isMacro && macrosTable[component.token] == null) {
-        let idx = component.token.match(/\d*/g)[0];
-        macrosTable.component.token = resolveChunk(idx);
+        let nextChunkIDX = component.token.slice(1, component.token.length - 1);
+        macrosTable[component.token] = resolveChunk(queryObj[nextChunkIDX]);
       }
     });
 
+    // Construct return set and calculate chunk
     let chunkResp = null;
-    switch (chunk.ops.operation) {
-      case 'and':
-        chunk.ops.components.map((component) => {
-          if (chunkResp == null) chunkResp = respTable[component.token];
-          else if (!component.isMacro)
-            chunkResp = chunkResp.intersection(respTable[component.token]);
-          else chunkResp = chunkResp.intersection(macrosTable[component.token]);
-        });
-      default:
-        break;
-    }
+    chunk.ops.components.map((component) => {
+      if (chunkResp == null) {
+        chunkResp = component.isMacro
+          ? macrosTable[component.token]
+          : respTable[component.token];
+        console.log('Set started');
+      } else {
+        let comparisonSet = component.isMacro
+          ? macrosTable[component.token]
+          : respTable[component.token];
 
+        if (comparisonSet == null) comparisonSet = new Set();
+
+        if (chunk.ops.operation == 'and')
+          chunkResp = chunkResp.intersection(comparisonSet);
+      }
+    });
     return chunkResp;
   };
 
@@ -151,4 +207,4 @@ const search = async (searchString) => {
   console.log(queryResp);
 };
 
-search('( React & vue ) & angular');
+search(' python & react ');
